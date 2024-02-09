44141f2bc0d2572d6e3f35984e03d464ca6f8f9c
Annania Plossl <nia.plossl@gmail.com>
2024-02-09 16:51:25 -0500

    completed the lab

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Code your solutions in this file
+function writeCards(names,gift){
+    let greetings=[];
+    for (let i = 0; i < names.length; i++) {
+        greetings.push(`Thank you, ${names[i]}, for the wonderful ${gift} gift!`)
+    };
+    return greetings;
+}
+function countDown(number) {
+    for (let i = number; i >= 0; i--) {
+        console.log(i);
+    }
+}
